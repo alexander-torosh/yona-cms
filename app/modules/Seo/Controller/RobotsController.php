5ff816ef4529f1eb31f8a199607f4152624404d5
Aleksandr Torosh <webtorua@gmail.com>
2014-08-26 17:19:09 +0300

    Added SEO module and robots.txt editor

--- app/modules/Seo/Controller/RobotsController.php
+++ app/modules/Seo/Controller/RobotsController.php
@@ -3,6 +3,7 @@
 namespace Seo\Controller;
 
 use Application\Mvc\Controller;
+use Seo\Form\RobotsForm;
 
 class RobotsController extends Controller
 {
@@ -18,9 +19,23 @@ class RobotsController extends Controller
 
     public function indexAction()
     {
+        $form = new RobotsForm();
 
         if ($this->request->isPost()) {
-            $file = $this->request->getPost('file');
+            if ($form->isValid()) {
+                $robots = $this->request->getPost('robots', 'string');
+                $result = file_put_contents($this->robotsFilePath, $robots);
+                if ($result) {
+                    $this->flash->success('Файл robots.txt обновлен');
+                    $this->redirect('/seo/robots');
+                } else {
+                    $this->flash->error('Ошибка! Файл robots.txt не обновлен. Проверьте права на запись файла.');
+                }
+            } else {
+                $this->flashErrors($form);
+            }
+
+            /*$file = $this->request->getPost('file');
             $w_file = file_put_contents($this->robotsFilePath, $file);
             if ($w_file !== false){
                 $this->flash->success('Файл robots.txt обновлен');
@@ -28,18 +43,26 @@ class RobotsController extends Controller
             } else {
                 $this->flash->error('Ошибка! Файл robots.txt не обновлен');
                 $this->view->file = $file;
-            }
+            }*/
 
         } else {
-            $r_file = file_get_contents($this->robotsFilePath);
-            if ($r_file !== false){
+            $robots = file_get_contents($this->robotsFilePath);
+            $form->get('robots')->setDefault($robots);
+            //$r_file = file_get_contents($this->robotsFilePath);
+            /*if ($r_file !== false){
                 $this->view->file = $r_file;
             } else {
                 $this->flash->error('Файл robots.txt ещё не создан или к нему нет доступа');
                 $this->view->file = '';
-                
-            }
+
+            }*/
         }
+
+        $title = 'Редактирование robots.txt';
+        $this->helper->title($title);
+        $this->view->title = $title;
+        $this->view->form = $form;
+
 
     }
 
