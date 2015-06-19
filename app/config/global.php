712f2f7a6834e98b5bb7665245f8cc792adcee30
Oleksandr Torosh <webtorua@gmail.com>
2015-06-19 13:21:19 +0300

    reupdate admin lang to en

--- app/config/global.php
+++ app/config/global.php
@@ -25,5 +25,5 @@ return [
     // Values: ru, en.
     // All translations contains in /app/modules/Cms/admin_translations in files with names ru.php, en.php.
     // To add another language you can create in this directory new file with name de.php and set 'admin_language' => 'de' it will works.
-    'admin_language' => 'ru',
+    'admin_language' => 'en',
 ];
